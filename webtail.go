69e26ca1d0b5bf6e42589a925426ccf10e19f74e
Baiju Muthukadan <baiju.m.mail@gmail.com>
2015-06-24 18:14:38 +0530

    check for existing file served

--- webtail.go
+++ webtail.go
@@ -99,9 +99,11 @@ func isExistingPath(a string, list []string) bool {
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
 	fn := r.FormValue("file")
-	//if !isExistingPath(fn, filenames) {
-	//	panic("File path doesn't exist in the original list")
-	//}
+	if !stdIn {
+		if !isExistingPath(fn, filenames) {
+			panic("File path doesn't exist in the original list")
+		}
+	}
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		if _, ok := err.(websocket.HandshakeError); !ok {
