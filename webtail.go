51a0c187800574146915b929e62e9f628c7891a4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-04-01 06:57:32 +0530

    write message asynchronously using goroutine
    Created a separate function to be used as a goroutine.

--- webtail.go
+++ webtail.go
@@ -10,10 +10,14 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+func writer(ws *websocket.Conn) {
+	ws.WriteMessage(websocket.TextMessage, []byte("hello"))
+	ws.Close()
+}
+
 func serveWs(w http.ResponseWriter, r *http.Request) {
 	ws, _ := upgrader.Upgrade(w, r, nil)
-	ws.WriteMessage(websocket.TextMessage, []byte("Hello"))
-	ws.Close()
+	go writer(ws)
 }
 
 func main() {
